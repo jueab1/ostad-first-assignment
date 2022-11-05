90bcdd8d6fabf342a9afc3a363fc26f15cc12445
jueab1 <hoque273@gmail.com>
2022-11-05 04:42:26 -0800

    first commit

---
+++ export.js
@@ -0,0 +1,11 @@
+let asiaList =[
+    {name: 'Bangladesh', code: 'Bd'},
+    {name:'India',code:'In'},
+    {name:'Pakistan',code:'Pak'},
+    {name:'China',code:'Ch'},
+    {name:'Afganistan',code:'Af'},
+    {name:'Hongkong',code:'Hk'},
+    {name:'Nepal',code:'Ne'},
+    {name:'Bhutan',code:'Bh'}
+]
+export {asiaList};
---
+++ import.js
@@ -0,0 +1,6 @@
+import {asiaList} from "./export.js"
+
+asiaList.map((item,i)=>{
+    let Details =`My export list is ${item.name}`
+    console.log(Details);
+})
